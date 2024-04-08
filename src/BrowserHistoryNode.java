df2a3bb4575e721ad2b6c81af5d3d0bd9dbd07b0
Althaf <althafrifath@gmail.com>
2024-04-08 22:46:19 +0530

    creation of nodes

---
+++ src/BrowserHistoryNode.java
@@ -0,0 +1,19 @@
+class BrowserHistoryNode {
+    String Page_Name;
+    String Page_Id;
+    String Date;
+    String Site_URL;
+    Boolean Bookmark;
+    BrowserHistoryNode next;
+    BrowserHistoryNode previous;
+
+    public BrowserHistoryNode(String Page_Name, String Page_Id, String Date, String Site_URL, Boolean Bookmark) {
+        this.Page_Name = Page_Name;
+        this.Page_Id = Page_Id;
+        this.Date = Date;
+        this.Site_URL = Site_URL;
+        this.Bookmark = Bookmark;
+        this.next = null;
+        this.previous = null;
+    }
+}
